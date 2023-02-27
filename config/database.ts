e3d9edb26ca3c98ccc4f08d96e7e027f8eae6b9a
ElvisGT <elvisgiltorres@gmail.com>
2023-02-27 05:19:27 -0500

    Modelo User

---
+++ config/database.ts
@@ -0,0 +1,16 @@
+import {Schema,model} from 'mongoose';
+
+const UserSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        required:true,
+    }
+});
+
+const UserModel = model('User',UserSchema);
+
+export {UserModel};
