623e342cbd1f82a9c1b9513c50fdf4ab506db0ef
ElvisGT <elvisgiltorres@gmail.com>
2023-02-27 04:20:02 -0500

    Rutas principales

--- models/server.ts
+++ models/server.ts
@@ -1,45 +1,53 @@
-import express from 'express'
-import cors from 'cors'
-import compression from 'compression'
-import helmet from 'helmet'
-import dontenv from 'dotenv'
-dontenv.config()
+import express from 'express';
+import cors from 'cors';
+import compression from 'compression';
+import helmet from 'helmet';
+import dontenv from 'dotenv';
+dontenv.config();
 import { PORT } from '../config/env.config';
+import {productsRoutes,storeRoutes,usersRoutes} from '../routes'
 
 
 export default class Server {
-    private static _instance:Server
-    private port:number
-    private app:express.Application
+    private static _instance:Server;
+    private port:number;
+    private app:express.Application;
+    private routesPath = {
+        store:'/api/v1/store',
+        users:'/api/v1/users',
+        products:'/api/v1/products'
+    };
     
     constructor(){
-        this.port = PORT
-        this.app = express()
+        this.port = PORT as number;
+        this.app = express();
 
         //middlewares
-        this.middlewares()
+        this.middlewares();
 
         //routes
-        this.routes()
-    }
+        this.routes();
+    };
 
     public static get instance(){
-        return this._instance || (this._instance = new Server)
-    }
+        return this._instance || (this._instance = new Server);
+    };
 
     middlewares(){
-        this.app.use(cors())
-        this.app.use(compression())
-        this.app.use(helmet())
-    }
+        this.app.use(cors());
+        this.app.use(compression());
+        this.app.use(helmet());
+    };
 
     routes(){
-
-    }
+        this.app.use(this.routesPath.products,productsRoutes);
+        this.app.use(this.routesPath.products,storeRoutes);
+        this.app.use(this.routesPath.products,usersRoutes);
+    };
 
     listen(){
         this.app.listen(this.port,() => {
-            console.log(`Server corriendo en el puerto: ${this.port}`)
-        })
-    }
-}
+            console.log(`Server corriendo en el puerto: ${this.port}`);
+        });
+    };
+};
---
+++ routes/index.ts
@@ -0,0 +1,7 @@
+import productsRoutes from '../routes/products';
+import usersRoutes from '../routes/users';
+import storeRoutes from '../routes/store';
+
+export {productsRoutes}
+export {usersRoutes}
+export {storeRoutes}
